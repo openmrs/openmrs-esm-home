a30e4e116d0081fb9d8ac5fe69a060888335d0e9
Joel Denning <joeldenning@gmail.com>
2019-08-25 17:56:01 -0600

    MF-31: Implementing home page with patient search link.

---
+++ src/home.component.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { match, Route } from "react-router";
+import HomeDashboard from "./dashboard/home-dashboard.component";
+import PatientSearch from "./patient-search/patient-search.component";
+
+export default function Home(props: HomeProps) {
+  return (
+    <main className="omrs-main-content">
+      <Route path={props.match.url} exact component={HomeDashboard} />
+      <Route
+        path={props.match.url + "/patient-search"}
+        component={PatientSearch}
+      />
+    </main>
+  );
+}
+
+type HomeProps = {
+  match: match;
+};
