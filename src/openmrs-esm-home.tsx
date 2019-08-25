4809d93143803cc471686ce89c24c8270472ab84
Fatma Ali <alifatma2019@gmail.com>
2019-08-25 21:33:20 +0300

    Init commit (#1)
    * Init commit
    
    * Review changes

---
+++ src/openmrs-esm-home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import singleSpaReact from "single-spa-react";
+import Root from "./root.component";
+
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Root
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
