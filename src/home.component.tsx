d30f855582bae0eadc58613ae3f2eadc3736d16e
Fatma Ali <alifatma2019@gmail.com>
2019-08-27 16:20:35 +0300

    Added patient search UI

--- src/home.component.tsx
+++ src/home.component.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { match, Route } from "react-router";
+import { maybe } from "kremling";
 import HomeDashboard from "./dashboard/home-dashboard.component";
 import PatientSearch from "./patient-search/patient-search.component";
 
 export default function Home(props: HomeProps) {
+  const isPatientSearch = () => {
+    return props.match.url.includes("patient-search");
+  };
+
   return (
-    <main className="omrs-main-content">
+    <main className={maybe("omrs-main-content", !isPatientSearch)}>
       <Route path={props.match.url} exact component={HomeDashboard} />
       <Route
         path={props.match.url + "/patient-search"}
--- src/patient-search/patient-search.component.tsx
+++ src/patient-search/patient-search.component.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { match } from "react-router";
+import styles from "./patient-search.component.css";
 
 export default function PatientSearch(props: PatientSearchProps) {
-  return <section>The patient search!</section>;
+  return (
+    <div>
+      <div className={styles.patientSearchHeader}>
+        <button onClick={props.history.goBack} className={styles.iconBtn}>
+          <svg className="omrs-icon" fill="var(--omrs-color-interaction)">
+            <use xlinkHref="#omrs-icon-arrow-back" />
+          </svg>
+        </button>
+        <input
+          className={`omrs-type-title-5 ${styles.patientSearchInput}`}
+          placeholder="Search for patient"
+          autoFocus
+        />
+      </div>
+      <div className="searchHelper">
+        <p className={`omrs-type-body-regular ${styles.helperText}`}>
+          Search by <span className="omrs-bold">patient number</span>
+        </p>
+        <p className={`omrs-type-body-regular ${styles.helperText}`}>
+          If unsuccessful, try patient name
+        </p>
+      </div>
+    </div>
+  );
 }
 
 type PatientSearchProps = {
-  match: match;
+  match?: match;
+  history?: any;
 };
