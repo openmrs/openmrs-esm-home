98107f5759508350af21067473665f1b051f3236
Anne Wachiya <wachiya84@gmail.com>
2019-09-17 11:13:35 +0300

    MF-50: Add authorization for displaying patient search button on home page

--- src/dashboard/home-dashboard.component.tsx
+++ src/dashboard/home-dashboard.component.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import styles from "./home-dashboard.component.css";
 import { Link, match } from "react-router-dom";
+import { UserHasAccessReact } from "@openmrs/esm-api";
 
 export default function HomeDashboard(props: HomeDashboardProps) {
   return (
     <div className={styles.homeDashboard}>
       <section className={styles.mainSection}>
-        <Link
-          to={props.match.url + "/patient-search"}
-          className="omrs-link omrs-outlined-action omrs-rounded"
-        >
-          <svg className="omrs-icon" fill="var(--omrs-color-interaction)">
-            <use xlinkHref="#omrs-icon-search" />
-          </svg>
-          <span className={styles.label}>Search for patient</span>
-        </Link>
+        <UserHasAccessReact privilege="View Patients">
+          <Link
+            to={props.match.url + "/patient-search"}
+            className="omrs-link omrs-outlined-action omrs-rounded"
+          >
+            <svg className="omrs-icon" fill="var(--omrs-color-interaction)">
+              <use xlinkHref="#omrs-icon-search" />
+            </svg>
+            <span className={styles.label}>Search for patient</span>
+          </Link>
+        </UserHasAccessReact>
       </section>
       <section className={styles.logoSection}>
         <svg>
--- src/openmrs-esm-home.d.tsx
+++ src/openmrs-esm-home.d.tsx
@@ -1,4 +1,5 @@
 declare module "*.css";
 declare module "@openmrs/esm-api" {
   export function openmrsFetch(url: string, fetchInit: any): any;
+  export function UserHasAccessReact(props: any): any;
 }
