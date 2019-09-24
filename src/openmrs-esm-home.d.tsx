fc47c8eeb0193b5a896506f3769d82eb91a0ef30
Joel Denning <joeldenning@gmail.com>
2019-09-24 02:06:54 -0600

    MF-66 and MF-67 (#11)
    * MF-66 and MF-67
    
    * Upgrading esm-api

--- src/openmrs-esm-home.d.tsx
+++
@@ -1,5 +0,0 @@
-declare module "*.css";
-declare module "@openmrs/esm-api" {
-  export function openmrsFetch(url: string, fetchInit: any): any;
-  export function UserHasAccessReact(props: any): any;
-}
