1a902e318f8e52d1d90638b01b1b2eddc808410c
Anne Wachiya <wachiya84@gmail.com>
2019-09-25 18:54:40 +0300

    Fixed search button width (#10)

--- src/dashboard/home-dashboard.component.tsx
+++ src/dashboard/home-dashboard.component.tsx
@@ -5,26 +5,28 @@ import { UserHasAccessReact } from "@openmrs/esm-api";
 
 export default function HomeDashboard(props: HomeDashboardProps) {
   return (
-    <div className={styles.homeDashboard}>
-      <section className={styles.mainSection}>
-        <UserHasAccessReact privilege="View Patients">
-          <Link
-            to={props.match.url + "/patient-search"}
-            className="omrs-link omrs-outlined-action omrs-rounded"
-          >
-            <svg className="omrs-icon" fill="var(--omrs-color-interaction)">
-              <use xlinkHref="#omrs-icon-search" />
-            </svg>
-            <span className={styles.label}>Search for patient</span>
-          </Link>
-        </UserHasAccessReact>
-      </section>
+    <>
+      <div className={styles.homeDashboard}>
+        <section className={styles.mainSection}>
+          <UserHasAccessReact privilege="View Patients">
+            <Link
+              to={props.match.url + "/patient-search"}
+              className={`omrs-link omrs-outlined-action omrs-rounded ${styles.dashboardLink}`}
+            >
+              <svg className="omrs-icon" fill="var(--omrs-color-interaction)">
+                <use xlinkHref="#omrs-icon-search" />
+              </svg>
+              <span className={styles.label}>Search for patient</span>
+            </Link>
+          </UserHasAccessReact>
+        </section>
+      </div>
       <section className={styles.logoSection}>
         <svg>
           <use xlinkHref="#omrs-logo-full-mono" />
         </svg>
       </section>
-    </div>
+    </>
   );
 }
 
