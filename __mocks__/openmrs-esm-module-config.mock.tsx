1d3c0aabd13f7632a28ce55e05083d7bcb4dd069
Brandon Istenes <bistenes@gmail.com>
2020-07-06 10:31:29 -0700

    Update module-config and tests

--- __mocks__/openmrs-esm-module-config.mock.tsx
+++ __mocks__/openmrs-esm-module-config.mock.tsx
@@ -4,8 +4,11 @@ export const defineConfigSchema = jest.fn();
 
 export const validators = {
   isBoolean: jest.fn(),
-  isString: jest.fn()
+  isString: jest.fn(),
+  isUrlWithTemplateParameters: jest.fn()
 };
+
+export const interpolateString = jest.fn().mockImplementation(s => s);
 
 export const useConfig = jest.fn().mockReturnValue({
   buttons: {
@@ -33,5 +36,11 @@ export const useConfig = jest.fn().mockReturnValue({
     }
   }
 });
+
+export const ConfigurableLink = props => (
+  <a href={props.to} {...props}>
+    {props.children}
+  </a>
+);
 
 export const ModuleNameContext = React.createContext("fake-module-config");
