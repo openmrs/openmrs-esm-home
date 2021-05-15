f2e9116f47dbc00d997f983fbab97456461c5c4b
Donald Kibet <chelashawddonald@yahoo.com>
2021-05-15 18:07:28 +0300

    feat(mf-463) : moved search to esm-primary-navigation

--- packages/esm-home-app/src/root.component.tsx
+++ packages/esm-home-app/src/root.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 import HomeDashboard from './dashboard/home-dashboard.component';
-import PatientSearch from './patient-search/patient-search.component';
 
 interface RootProps {
   canSearch: boolean;
@@ -11,11 +10,6 @@ const Root: React.FC<RootProps> = ({ canSearch = true }) => (
   <BrowserRouter basename={window.spaBase}>
     <main className="omrs-main-content">
       <Route path="/home" exact component={(props) => <HomeDashboard canSearch={canSearch} {...props} />} />
-      {canSearch ? (
-        <Route path="/home/patient-search" component={PatientSearch} />
-      ) : (
-        <Redirect from="/home/patient-search" to="/home" />
-      )}
     </main>
   </BrowserRouter>
 );
