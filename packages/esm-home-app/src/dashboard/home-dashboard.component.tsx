7d348d88884bd4cde0545ef436ef62b505b0b8a1
Pius Rubangakene <piruville@gmail.com>
2022-07-19 08:50:52 +0300

    Upgrade to react router v6 (#92)
    * upgrade esm-home to react router v6
    
    * updates ci to use node 16.x

--- packages/esm-home-app/src/dashboard/home-dashboard.component.tsx
+++ packages/esm-home-app/src/dashboard/home-dashboard.component.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { match } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useConfig, ExtensionSlot, Extension } from '@openmrs/esm-framework';
 import DashboardButton from '../dashboard-button/dashboard-button.component';
 import styles from './home-dashboard.component.css';
 
 export interface HomeDashboardProps {
-  match: match;
   canSearch: boolean;
 }
 
-export default function HomeDashboard({ match, canSearch }: HomeDashboardProps) {
+export default function HomeDashboard({ canSearch }: HomeDashboardProps) {
   const config = useConfig();
   const { t } = useTranslation();
 
