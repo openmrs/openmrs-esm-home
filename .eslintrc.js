7be305da7bd6a5fdc9a9ebef0af85002e802bcaa
Zac Butko <zac.butko@gmail.com>
2022-09-02 02:04:07 -0700

    (chore) Update Yarn (#99)
    * first yarn commit + prettier
    
    * Remove unused packages, update jest, tests passing
    
    * fix pre-push
    
    * Small cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'ts-react-important-stuff',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+};
--- packages/esm-home-app/src/helpers/render-with-router.tsx
+++ packages/esm-home-app/src/helpers/render-with-router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
