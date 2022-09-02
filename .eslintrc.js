7d19db92542c9f13406720a154e80d465c6a4686
Zac Butko <zac.butko@gmail.com>
2022-09-01 20:34:20 -0700

    first yarn commit + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'ts-react-important-stuff',
+    'plugin:prettier/recommended',
+    'react-app',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+};
