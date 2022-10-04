18c1acfe8016d611635a7721c1eb550e3eb1b522
Zac Butko <zac.butko@gmail.com>
2022-10-04 11:17:21 -0700

    Exports version (#104)

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'ts-react-important-stuff',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-};
