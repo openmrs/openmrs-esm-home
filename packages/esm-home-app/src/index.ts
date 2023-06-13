9f7834eab8ea3ef2fc0e02a3b30533a3ff27b199
Dennis Kigen <kigen.work@gmail.com>
2023-06-13 22:59:46 +0300

    (BREAKING) Migrate `esm-home` to use routes.json

--- packages/esm-home-app/src/index.ts
+++ packages/esm-home-app/src/index.ts
@@ -3,25 +3,28 @@ import { createDashboardLink } from './createDashboardLink';
 import { dashboardMeta } from './dashboard.meta';
 import { esmHomeSchema } from './openmrs-esm-home-schema';
 
-declare var __VERSION__: string;
-// __VERSION__ is replaced by Webpack with the version from package.json
-const version = __VERSION__;
+const moduleName = '@openmrs/esm-home-app';
+const pageName = 'home';
 
-const backendDependencies = {
-  'webservices.rest': '^2.24.0',
+const options = {
+  featureName: pageName,
+  moduleName,
 };
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+export const importTranslation = require.context('../translations', true, /.json$/, 'lazy');
 
-function setupOpenMRS() {
-  const moduleName = '@openmrs/esm-home-app';
-  const pageName = 'home';
+export const root = getAsyncLifecycle(() => import('./root.component'), options);
 
-  const options = {
-    featureName: pageName,
-    moduleName,
-  };
+export const homeNavMenu = getAsyncLifecycle(() => import('./side-menu/side-menu.component'), options);
 
+export const homeWidgetDbLink = getSyncLifecycle(createDashboardLink(dashboardMeta), options);
+
+export const homeWidgetDashboard = getAsyncLifecycle(
+  () => import('./home-page-widgets/home-page-widgets.component'),
+  options,
+);
+
+export function startupApp() {
   defineConfigSchema(moduleName, esmHomeSchema);
 
   registerBreadcrumbs([
@@ -30,46 +33,4 @@ function setupOpenMRS() {
       title: 'Home',
     },
   ]);
-
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import('./root.component'), options),
-        route: pageName,
-        online: {
-          canSearch: true,
-        },
-        offline: {
-          canSearch: false,
-        },
-      },
-    ],
-    extensions: [
-      {
-        id: 'home-nav-menu',
-        slot: 'home-sidebar-slot',
-        load: getAsyncLifecycle(() => import('./side-menu/side-menu.component'), options),
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'home-widget-db-link',
-        slot: 'homepage-dashboard-slot',
-        load: getSyncLifecycle(createDashboardLink(dashboardMeta), options),
-        meta: dashboardMeta,
-        online: true,
-        offline: true,
-        order: 0,
-      },
-      {
-        id: 'home-widget-dashboard',
-        slot: 'home-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./home-page-widgets/home-page-widgets.component'), options),
-        online: true,
-        offline: true,
-      },
-    ],
-  };
 }
-
-export { backendDependencies, importTranslation, setupOpenMRS, version };
