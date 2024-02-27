34ac8f9ab71e9814c1cc7b6ba8db78b0628bdf95
arodidev <51090527+arodidev@users.noreply.github.com>
2024-02-27 14:32:34 +0300

    home page header

--- packages/esm-home-app/src/home-page-widgets/home-page-widgets.component.tsx
+++ packages/esm-home-app/src/home-page-widgets/home-page-widgets.component.tsx
@@ -1,11 +1,20 @@
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
 import styles from './home-page-widgets.scss';
+import HomepageHeader from '../homepage-header/homepage-header.component';
+import { useTranslation } from 'react-i18next';
 
 interface HomePageWidgetsProps {}
 
 const HomePageWidgets: React.FC<HomePageWidgetsProps> = () => {
-  return <ExtensionSlot className={styles.homePageWidget} name="homepage-widgets-slot" />;
+  const { t } = useTranslation();
+
+  return (
+    <React.Fragment>
+      <HomepageHeader title={t('home', 'Home')} />
+      <ExtensionSlot className={styles.homePageWidget} name="homepage-widgets-slot" />
+    </React.Fragment>
+  );
 };
 
 export default HomePageWidgets;
---
+++ packages/esm-home-app/src/homepage-header/homepage-header.component.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Location } from '@carbon/react/icons';
+import { useSession } from '@openmrs/esm-framework';
+import { DatePicker, DatePickerInput } from '@carbon/react';
+import dayjs from 'dayjs';
+import styles from './homepage-header.scss';
+import HomepageIllustration from './appointments-illustration.component';
+
+interface HomepageHeaderProps {
+  title: string;
+}
+
+const HomepageHeader: React.FC<HomepageHeaderProps> = ({ title }) => {
+  const { t } = useTranslation();
+  const session = useSession();
+  const location = session?.sessionLocation?.display;
+
+  return (
+    <div className={styles.header} data-testid="homepage-header">
+      <div className={styles['left-justified-items']}>
+        <HomepageIllustration />
+        <div className={styles['page-labels']}>
+          <p>{t('home', 'Home')}</p>
+          <p className={styles['page-name']}>{title}</p>
+        </div>
+      </div>
+      <div className={styles['right-justified-items']}>
+        <div className={styles['date-and-location']}>
+          <Location size={16} />
+          <span className={styles.value}>{location}</span>
+          <span className={styles.middot}>&middot;</span>
+          <DatePicker dateFormat="d-M-Y" datePickerType="single">
+            <DatePickerInput
+              style={{ backgroundColor: 'transparent', border: 'none', maxWidth: '10rem' }}
+              id="date-picker-calendar-id"
+              placeholder="DD-MMM-YYYY"
+              labelText=""
+              type="text"
+              value={dayjs(new Date()).format('DD MMM YYYY')}
+            />
+          </DatePicker>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomepageHeader;
