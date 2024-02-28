1ac66bca5fd40a6295632ceb040de247c12471fa
arodidev <51090527+arodidev@users.noreply.github.com>
2024-02-28 12:42:27 +0300

    load header via extension

--- packages/esm-home-app/src/home-page-widgets/home-page-widgets.component.tsx
+++ packages/esm-home-app/src/home-page-widgets/home-page-widgets.component.tsx
@@ -1,20 +1,11 @@
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
 import styles from './home-page-widgets.scss';
-import HomepageHeader from '../homepage-header/homepage-header.component';
-import { useTranslation } from 'react-i18next';
 
 interface HomePageWidgetsProps {}
 
 const HomePageWidgets: React.FC<HomePageWidgetsProps> = () => {
-  const { t } = useTranslation();
-
-  return (
-    <React.Fragment>
-      <HomepageHeader title={t('home', 'Home')} />
-      <ExtensionSlot className={styles.homePageWidget} name="homepage-widgets-slot" />
-    </React.Fragment>
-  );
+  return <ExtensionSlot className={styles.homePageWidget} name="homepage-widgets-slot" />;
 };
 
 export default HomePageWidgets;
