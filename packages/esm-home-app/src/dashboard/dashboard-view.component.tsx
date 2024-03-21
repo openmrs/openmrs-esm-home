e3983e1f9991d188963d2dd5d012d823539552fa
arodidev <51090527+arodidev@users.noreply.github.com>
2024-03-21 14:00:10 +0300

    fix extension slot misplacement

--- packages/esm-home-app/src/dashboard/dashboard-view.component.tsx
+++ packages/esm-home-app/src/dashboard/dashboard-view.component.tsx
@@ -5,7 +5,6 @@ import styles from './dashboard-view.scss';
 const DashboardView: React.FC<{ dashboardSlot: string; title: string }> = ({ dashboardSlot, title }) => {
   return (
     <>
-      <ExtensionSlot name="home-metrics-widgets-slot" />
       <ExtensionSlot className={styles.dashboardView} name={dashboardSlot} state={{ dashboardTitle: title }} />;
     </>
   );
---
+++ packages/esm-home-app/src/metrics/metrics.component.tsx
@@ -0,0 +1,8 @@
+import { ExtensionSlot } from '@openmrs/esm-framework';
+import React from 'react';
+
+const PageMetrics: React.FC = () => {
+  return <ExtensionSlot name="home-metrics-tiles-slot" />;
+};
+
+export default PageMetrics;
