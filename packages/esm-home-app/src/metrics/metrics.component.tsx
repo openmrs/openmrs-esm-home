09212dda246852f966f445d85b8d372ec21f22e2
arodidev <51090527+arodidev@users.noreply.github.com>
2024-03-22 11:31:47 +0300

    added flex to extension

--- packages/esm-home-app/src/metrics/metrics.component.tsx
+++ packages/esm-home-app/src/metrics/metrics.component.tsx
@@ -1,8 +1,9 @@
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
+import styles from './metrics.scss';
 
 const Metrics: React.FC = () => {
-  return <ExtensionSlot name="home-metrics-tiles-slot" />;
+  return <ExtensionSlot className={styles.metrics} name="home-metrics-tiles-slot" />;
 };
 
 export default Metrics;
