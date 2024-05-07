585956478b8102d06d46ef5233346e54bf243ae7
Brandon Istenes <bistenes@gmail.com>
2024-05-07 13:40:56 -0400

    (feat) O3-2724: Move overlays into the framework (#201)
    * Set up workspace overlay on home page
    
    * Fixup
    
    * Upgrade core

--- packages/esm-home-app/src/root.component.tsx
+++ packages/esm-home-app/src/root.component.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { setLeftNav, unsetLeftNav } from '@openmrs/esm-framework';
+import { WorkspaceOverlay, setLeftNav, unsetLeftNav } from '@openmrs/esm-framework';
 import HomeDashboard from './dashboard/home-dashboard.component';
 
 const Root: React.FC = () => {
@@ -12,14 +12,17 @@ const Root: React.FC = () => {
   }, [spaBasePath]);
 
   return (
-    <BrowserRouter basename={window.spaBase}>
-      <main className="omrs-main-content">
-        <Routes>
-          <Route path="/home" element={<HomeDashboard />} />
-          <Route path="/home/:dashboard/*" element={<HomeDashboard />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename={window.spaBase}>
+        <main className="omrs-main-content">
+          <Routes>
+            <Route path="/home" element={<HomeDashboard />} />
+            <Route path="/home/:dashboard/*" element={<HomeDashboard />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+      <WorkspaceOverlay contextKey="home" />
+    </>
   );
 };
 
