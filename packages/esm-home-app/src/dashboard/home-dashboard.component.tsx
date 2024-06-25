7c8cc7ec33a1be36b04d8370c5e51e09bb4889f9
Brandon Istenes <bistenes@gmail.com>
2024-06-25 10:48:35 -0400

    (fix) Improvements to workspace system styling (supports O3-3246) (#208)

--- packages/esm-home-app/src/dashboard/home-dashboard.component.tsx
+++ packages/esm-home-app/src/dashboard/home-dashboard.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useLayoutType, isDesktop, useExtensionStore, ExtensionSlot } from '@openmrs/esm-framework';
+import { useLayoutType, isDesktop, useExtensionStore, ExtensionSlot, WorkspaceContainer } from '@openmrs/esm-framework';
 import DashboardView from './dashboard-view.component';
 import type { DashboardConfig } from '../types/index';
 import styles from './home-dashboard.scss';
@@ -17,9 +17,12 @@ export default function HomeDashboard() {
   const activeDashboard = dashboards.find((dashboard) => dashboard.name === params?.dashboard) || dashboards[0];
 
   return (
-    <section className={isDesktop(layout) && styles.dashboardContainer}>
-      {isDesktop(layout) && <ExtensionSlot name="home-sidebar-slot" key={layout} />}
-      <DashboardView title={activeDashboard?.name} dashboardSlot={activeDashboard?.slot} />
-    </section>
+    <div className={styles.homePageWrapper}>
+      <section className={isDesktop(layout) ? styles.dashboardContainer : styles.dashboardContainerTablet}>
+        {isDesktop(layout) && <ExtensionSlot name="home-sidebar-slot" key={layout} />}
+        <DashboardView title={activeDashboard?.name} dashboardSlot={activeDashboard?.slot} />
+      </section>
+      <WorkspaceContainer overlay contextKey="home" />
+    </div>
   );
 }
