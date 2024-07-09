9e97b6bbc02e9c8f88734d3da338534b80335898
arodidev <51090527+arodidev@users.noreply.github.com>
2024-07-09 20:53:39 +0300

    add styles to extension slot

--- packages/esm-home-app/src/metrics/metrics.component.tsx
+++ packages/esm-home-app/src/metrics/metrics.component.tsx
@@ -1,8 +1,9 @@
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import React from 'react';
+import styles from './metrics.scss';
 
 const Metrics: React.FC = () => {
-  return <ExtensionSlot name="home-metrics-tiles-slot" />;
+  return <ExtensionSlot name="home-metrics-tiles-slot" className={styles.container} />;
 };
 
 export default Metrics;
