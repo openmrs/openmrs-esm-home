38120090f91989eb946466a37f2b8930c9b8cab8
Dennis Kigen <kigen.work@gmail.com>
2024-07-30 13:30:48 +0300

    (fix) Restore metrics tile slot classname

--- packages/esm-home-app/src/metrics/metrics.component.tsx
+++ packages/esm-home-app/src/metrics/metrics.component.tsx
@@ -3,7 +3,7 @@ import { ExtensionSlot } from '@openmrs/esm-framework';
 import styles from './metrics.scss';
 
 const Metrics: React.FC = () => {
-  return <ExtensionSlot name="home-metrics-tiles-slot" className={styles.container} />;
+  return <ExtensionSlot name="home-metrics-tiles-slot" className={styles.metricTilesSlot} />;
 };
 
 export default Metrics;
