9cbd374ce914082784350e664e44872d1b6c38c0
Dennis Kigen <kigen.work@gmail.com>
2025-04-29 11:56:59 +0300

    (chore) Add placeholder react-aria type declarations

--- packages/esm-home-app/src/declarations.d.ts
+++ packages/esm-home-app/src/declarations.d.ts
@@ -3,3 +3,16 @@ declare module '*.scss';
 declare module '*.png';
 
 declare interface SideNavProps {}
+
+declare module 'react-aria' {
+  export const I18nProvider: (...args: unknown) => JSX.Element;
+  export type { DateValue } from '@react-types/datepicker';
+  export const mergeProps: unknown;
+  export const useLocale: unknown;
+  export const useDateField: unknown;
+  export const useDatePicker: unknown;
+  export const useDateSegment: unknown;
+  export const useFocusRing: unknown;
+  export const useHover: unknown;
+  export const useObjectRef: unknown;
+}
