87c880a43307e45ef58c06475ce884f96ce8d616
Dennis Kigen <kigen.work@gmail.com>
2025-05-01 00:31:27 +0300

    (chore) Update OpenMRS dependencies

--- packages/esm-home-app/src/declarations.d.ts
+++ packages/esm-home-app/src/declarations.d.ts
@@ -1,19 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.css';
 declare module '*.scss';
 declare module '*.png';
 
 declare interface SideNavProps {}
-
-declare module 'react-aria' {
-  export const I18nProvider: (...args: any) => JSX.Element;
-  export type { DateValue } from '@react-types/datepicker';
-  export const mergeProps: any;
-  export const useLocale: any;
-  export const useDateField: any;
-  export const useDatePicker: any;
-  export const useDateSegment: any;
-  export const useFocusRing: any;
-  export const useHover: any;
-  export const useObjectRef: any;
-}
